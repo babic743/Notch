5e1339e0b8d7445984e3075be03abcbc063561b9
babicjosipa <josipa.babic@ag04.com>
2024-06-24 11:18:49 +0200

    contact form test added

---
+++ cypress/e2e/contactForm.spec.cy.js
@@ -0,0 +1,38 @@
+/// <reference types="cypress" />
+import HomePage from "../pageObjects/homePage";
+
+function generateRandomString(length) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+describe("Homepage Tests", () => {
+  const homePage = new HomePage();
+
+  beforeEach(() => {
+    cy.visit("/");
+    homePage.acceptCookies();
+  });
+
+  it("Should click on Contact us button and fill in form", () => {
+    cy.get("li.featured.menu-item.menu-contact-us > a").click();
+    cy.wait(3000);
+    cy.contains("How can we help? Let us know.");
+    const randomText = generateRandomString(10);
+    cy.get("#firstname").type(randomText);
+    cy.get("#lastname").type(randomText);
+    cy.get("#email").type("josipa@gmail.com");
+    cy.get("#source").select("google");
+    cy.get('label[for="Product Discovery"]').click();
+    cy.get("#budget").select("€50.000 to €100.000");
+    cy.get('label[for="pp"]').click();
+    cy.get('button[type="submit"].js-contact-form-submit')
+      .should("be.visible")
+      .should("be.enabled");
+  });
+});
