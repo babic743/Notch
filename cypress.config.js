9f233772f51e68cf9c01b1a3b767495eb4a0c15f
Zvonimir Knezevic <zvonimir.knezevic@beeline.com>
2024-07-18 13:51:38 +0200

    test1

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,6 @@ module.exports = defineConfig({
     baseUrl: "https://wearenotch.com/",
     watchForFileChanges: false,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
