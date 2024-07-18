175178ae6f9e7611e1e67ffe9f273e9db464262a
babicjosipa <josipa.babic@ag04.com>
2024-07-18 11:27:08 +0200

    new changes

--- cypress/pageObjects/homePage.js
+++ cypress/pageObjects/homePage.js
@@ -10,6 +10,11 @@ class HomePage {
       .each(($el) => {
         cy.wrap($el).click({ force: true }); // Click each element found
       });
+    cy.get("#cookie_action_close_header", { timeout: 10000 }) // Increase timeout if needed
+      .should("be.visible")
+      .each(($el) => {
+        cy.wrap($el).click({ force: true }); // Click each element found
+      });
   }
 }
 
